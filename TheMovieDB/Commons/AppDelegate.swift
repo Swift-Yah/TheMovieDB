d445f654b0d6e4a49163be19f35a80f14d3c1c87
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2017-10-30 13:14:55 -0200

    Organize project files into folder and remove boilerplace code

---
+++ TheMovieDB/Commons/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  TheMovieDB
+//
+//  Created by Rafael Ferreira on 10/30/17.
+//  Copyright © 2017 Swift Yah. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
---
+++ TheMovieDB/Controllers/HomeViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewController.swift
+//  TheMovieDB
+//
+//  Created by Rafael Ferreira on 10/30/17.
+//  Copyright © 2017 Swift Yah. All rights reserved.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+}
